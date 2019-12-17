759450e26f0858e4f4742afe4d6b234aa9c265a1
jhc <1753786027@qq.com>
2019-12-17 19:26:41 +0800

    [add] 新增下载模块

---
+++ Bdownload/interface.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# coding=utf-8
+"""
+@module         main/interface 对外接口
+@author         jhc
+@time           2019/12/12
+"""
+
+import os
+import sys
+import random
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
